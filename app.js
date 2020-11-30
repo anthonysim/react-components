58fd6edb4c2fb8672b2254df72137f900ebdaa99
Anthony Sim <anthonyysim@gmail.com>
2020-11-30 11:48:33 -0800

    Complete Rect Components

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-// TODO
+var GroceryList = () => {
+  let items = ['milk', 'cheese'];
+  let groceries = items.map((item, i) => <GroceryListItem key={i}>{item}</GroceryListItem>)
+
+  return (
+    <div>
+      {groceries}
+    </div>
+  );
+}
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false
+    }
+  }
+
+
+  render() {
+    const boldHandler = () => {
+      this.setState({
+        hover: true
+    })
+  }
+
+  const normalHandler = () => {
+    this.setState({
+      hover: false
+  })
+}
+
+    let style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    }
+
+    return (
+      <div>
+        <ul>
+        <li
+          style={style}
+          onMouseOver={boldHandler}
+          onMouseOut={normalHandler}
+        >
+          {this.props.children}
+        </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'))
+
+
